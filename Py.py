62e2a5eead548fb38ba778f14a5b0db38a703fb6
Dedan Kimathi Odira <136998941+Mufasa-DedanKimathi@users.noreply.github.com>
2024-10-30 15:45:28 +0300

    Create Py.py

---
+++ Py.py
@@ -0,0 +1,34 @@
+### Import math functions.
+
+import math
+x = math.sqrt(25)
+print(x)
+import math as m
+y = m.sqrt(50)
+print(y)
+from math import pow,sqrt
+print(pow(3,5))
+print(sqrt(125))
+
+print(math.floor(2.9))
+print(math.ceil(2.9))
+
+print(math.pow(3,2))
+print(math.pi)
+print(math.e)
+help(math)
+
+###input fucntion
+
+name = input('Enter your name here')
+ID = input('type your ID number here')
+Address = input('Help us know your location')
+print(name,ID,Address)
+
+
+
+
+
+
+
+
